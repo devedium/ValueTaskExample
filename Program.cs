f4233fd074c14eb5832fdc8bc0b4cbe56b519444
devedium <devedium@gmail.com>
2023-05-15 14:40:07 -0400

    simple version

--- Program.cs
+++ Program.cs
@@ -1,77 +1,72 @@
-﻿namespace ValueTaskExample
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+public class Program
 {
-    using System;
-    using System.Diagnostics;
-    using System.Threading.Tasks;
-
-    public class Program
+    public static async Task Main()
     {
-        public static async Task Main()
+        long limit = 2;
+        var stopwatch = new Stopwatch();
+
+        Console.WriteLine("Press 'C' key to exit the loop...");
+
+        while (true)
         {
-            long limit = 2;
-            bool enteredNoGCRegion = false;
-
-            enteredNoGCRegion = GC.TryStartNoGCRegion((int.MaxValue) / 10);
-            var stopwatch = new Stopwatch();
-            while (true)
+            if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.C)
             {
-                limit++;
-                stopwatch.Start();
-
-                // Call the asynchronous method  2_147_483_600
-                long maxPrime = await MaxPrimeLessThanAsync(limit);
-
-                stopwatch.Stop();
-
-                // Print the result
-                Console.Write($"Max prime less than {limit,10} is {maxPrime,10} ");
-
-                Console.Write($"task took {stopwatch.ElapsedMilliseconds,5} ms.");
-                // Print the current memory usage
-                long memoryUsed = Process.GetCurrentProcess().WorkingSet64;
-                Console.Write($" memory usage: {memoryUsed: 10}\r");
+                break;
             }
-            if (enteredNoGCRegion)
+
+            limit++;
+            stopwatch.Start();
+
+            // Call the asynchronous method  2_147_483_600
+            var maxPrime = await MaxPrimeLessThanAsync(limit);
+            
+            stopwatch.Stop();
+
+            // Print the result
+            Console.Write($"Max prime less than {limit,10} is {maxPrime,10} ");
+
+            Console.Write($"task took {stopwatch.ElapsedMilliseconds,5} ms.\r");
+        }
+    }
+
+    public static  ValueTask<long> MaxPrimeLessThanAsync(long number)
+    {
+        if (number < 2_000_000_000)
+        {
+            return new ValueTask<long>(MaxPrimeLessThan(number));
+        }
+        else
+        {
+            return new ValueTask<long>(Task.Run(() => MaxPrimeLessThan(number)));
+        }
+    }
+
+    private static long MaxPrimeLessThan(long n)
+    {
+        for (long i = n - 1; i >= 2; i--)
+        {
+            if (IsPrime(i))
             {
-                GC.EndNoGCRegion();
+                return i;
             }
         }
 
-        public static Task<long> MaxPrimeLessThanAsync(long number)
+        return -1; // Return -1 if no prime found (i.e., when n is less than 2)
+    }
+
+    private static bool IsPrime(long n)
+    {
+        if (n < 2) return false;
+
+        for (int i = 2; i * i <= n; i++)
         {
-            if (number < 2_000_000_000)
-            {
-                return Task.FromResult(MaxPrimeLessThan(number));
-            }
-            else
-            {
-                return Task.Run(() => MaxPrimeLessThan(number));
-            }
+            if (n % i == 0) return false;
         }
 
-        private static long MaxPrimeLessThan(long n)
-        {
-            for (long i = n - 1; i >= 2; i--)
-            {
-                if (IsPrime(i))
-                {
-                    return i;
-                }
-            }
-
-            return -1; // Return -1 if no prime found (i.e., when n is less than 2)
-        }
-
-        private static bool IsPrime(long n)
-        {
-            if (n < 2) return false;
-
-            for (int i = 2; i * i <= n; i++)
-            {
-                if (n % i == 0) return false;
-            }
-
-            return true;
-        }
+        return true;
     }
 }
